13a76947be079d41037e641090ef48e3a8b4e9dc
Shiyu Liu <shiyu.liu27@outlook.com>
2025-07-13 19:46:16 +0200

    implement proper installation

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'jacobi_robot'
@@ -10,6 +12,14 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Install all launch files
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        # Install all files in urdf/ and subfolders
+        *[
+            (os.path.join('share', package_name, os.path.dirname(path)), [path])
+            for path in glob('urdf/**/*', recursive=True)
+            if os.path.isfile(path)
+        ],
     ],
     install_requires=['setuptools'],
     zip_safe=True,
